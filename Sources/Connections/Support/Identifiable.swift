71dfb8809b0f557356cb12b4d88040179d1dd851
nkalvi <nkalvi@users.noreply.github.com>
2023-07-23 22:00:14 -0700

    Adopt to changes in iOS17, macOS14
    Identifiable conformance added to Contacts in iOS17, macOS14

--- Sources/Connections/Support/Identifiable.swift
+++ Sources/Connections/Support/Identifiable.swift
@@ -1,8 +1,14 @@
 import Foundation
 import Contacts
 
-extension CNContact {
-    public var id: String { identifier }
+@available(iOS, obsoleted: 16, message: "Contacts module includes it in iOS17+, macOS14+")
+@available(macOS, obsoleted: 14, message: "Contacts module includes it in iOS17+, macOS14+")
+extension CNContact: Identifiable {
+    public typealias ID = UUID
+    public var id: UUID {
+        UUID(uuidString: identifier.replacingOccurrences(of: ":ABPerson", with: ""))! 
+    }
+
 }
 
 extension CNGroup: Identifiable {
